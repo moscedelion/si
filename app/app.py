6625d80246d44fa036dcc62ccbe29d9f31037a10
moscedelion <asiman-gasanoff@yandex.ru>
2020-04-18 00:45:29 +0500

    add templates

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 from flask import Flask
+from flask import render_template
+from jinja2 import Environment, PackageLoader, select_autoescape
 
+env = Environment(
+    loader=PackageLoader(__name__, 'templates'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
 app = Flask(__name__)
 
 @app.route('/')
 def main():
-	return 'Hello, World'
+	template = env.get_template('app.html')
+	return template.render()
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
